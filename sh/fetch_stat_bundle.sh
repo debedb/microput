6834d869c4848bbe2489075d8202935615ecce64
Gregory Golberg <grisha@alum.mit.edu>
2014-08-19 06:53:07 +0000

    typos

--- sh/fetch_stat_bundle.sh
+++ sh/fetch_stat_bundle.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/opt/enr/virtupy/bin/python /opt/enr/py/fetch_stat_bundle.sh  2>&1 >> /opt/enr/log/fetch_stat_bundle.sh
+/opt/enr/virtupy/bin/python /opt/enr/py/fetch_stat_bundle.py  2>&1 >> /opt/enr/log/fetch_stat_bundle.log
